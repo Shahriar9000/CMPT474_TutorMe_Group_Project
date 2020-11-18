d821b7135451af114b440a26c31bf2d79de85719
marefin <marefin@sfu.ca>
2020-11-17 22:05:46 -0800

    added my api

--- src/actions/teacherClickedActions.js
+++ src/actions/teacherClickedActions.js
@@ -2,7 +2,7 @@ import { FETCH_TEACHER_CLICKED } from './types';
 
 
 export  const findTeacherClicked = inputData => dispatch => {
-  fetch('https://7yqu7y8fs3.execute-api.us-east-1.amazonaws.com/dev/getTutor/username/' + inputData, {
+  fetch('https://ksc2toqk8k.execute-api.us-east-1.amazonaws.com/dev/getTutor/username/' + inputData, {
     method: 'GET'
   })
     .then(res => res.json())
@@ -17,7 +17,7 @@ export  const findTeacherClicked = inputData => dispatch => {
 };
 
 export  const findStudentClicked = inputData => dispatch => {
-  fetch('https://7yqu7y8fs3.execute-api.us-east-1.amazonaws.com/dev/getStudent/username/' + inputData, {
+  fetch('https://ksc2toqk8k.execute-api.us-east-1.amazonaws.com/dev/getStudent/username/' + inputData, {
     method: 'GET'
   })
     .then(res => res.json())
