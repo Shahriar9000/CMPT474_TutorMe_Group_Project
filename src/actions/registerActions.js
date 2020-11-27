d5956845c3ce6af5d0682c2b9fcc2ede3f43e25f
marslan <muhammadarslan1502@gmail.com>
2020-11-26 21:17:07 -0800

    express to microservices

--- src/actions/registerActions.js
+++ src/actions/registerActions.js
@@ -2,7 +2,7 @@ import { FETCH_STUDENT } from './types';
 
 
 export  const registerStudent = inputData => dispatch => {
-  fetch('https://7yqu7y8fs3.execute-api.us-east-1.amazonaws.com/dev/add', {
+  fetch('https://thbrt8c830.execute-api.us-east-1.amazonaws.com/dev/add', {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
